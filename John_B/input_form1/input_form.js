8c5568a97378adccdc5081d927af0e09c4a55b8b
Richard Heaton <richard.heaton@coxautoinc.com>
2018-12-29 11:33:51 -0500

    homework assistance

--- John_B/input_form1/input_form.js
+++ John_B/input_form1/input_form.js
@@ -2,7 +2,7 @@ function validateForm() {
   //var x = document.forms["myForm"]["fname"].value;
 //  var x = document.getElementbyId("formGroupExampleInput").value;
   console.log(document);
-  //console.log(document.getElementbyId("formGroupExampleInput"));
+  console.log(document.getElementById("formGroupExampleInput"));
   /*if (x == "") {
     alert("Name must be filled out");
     return false;
