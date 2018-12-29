4b48b81c36eea92818326864e3beb3d2ad59bed9
john black <Johnb@bballin.com>
2018-12-29 11:21:33 -0500

    Input form with external js

---
+++ John_B/input_form1/input_form.js
@@ -0,0 +1,10 @@
+function validateForm() {
+  //var x = document.forms["myForm"]["fname"].value;
+//  var x = document.getElementbyId("formGroupExampleInput").value;
+  console.log(document);
+  //console.log(document.getElementbyId("formGroupExampleInput"));
+  /*if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }*/
+}
